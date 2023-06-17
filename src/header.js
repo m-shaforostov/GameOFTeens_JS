4e0044e552d62665d7f02d5f1d8130578cf828bc
maxim <maxim.shaforostov@gmail.com>
2023-06-17 22:50:54 +0300

    Second day adaptive header

--- src/header.js
+++ src/header.js
@@ -1,10 +1,11 @@
 import './header.css';
 import { useNavigate } from 'react-router-dom';
+import {useState} from "react";
 
 function Header() {
     const navigate = useNavigate();
     const navigFunc = (path) => () => navigate(path);
-
+    const [activeMenu, setActiveMenu] = useState(false);
     return (
         <div className="header-main">
             <div className="header-logo">
@@ -12,18 +13,30 @@ function Header() {
             </div>
             <div className="header-buttons">
                 <div className="ui secondary menu">
-                    <a className="item" href="#" onClick={ navigFunc('/startups') }>Startups</a>
-                    <a className="item" href="#" onClick={ navigFunc('/create-new-startup') }>Create new</a>
-                    <a className="item" href="#" onClick={ navigFunc('/news') }>News</a>
-                    <div className="right menu">
+                    <a className="item" id="link-button-1" href="#" onClick={ navigFunc('/startups') }>Startups</a>
+                    <a className="item" id="link-button-2" href="#" onClick={ navigFunc('/create-new-startup') }>Create new</a>
+                    <a className="item" id="link-button-3" href="#" onClick={ navigFunc('/news') }>News</a>
+                    <div className="right menu" id="search-input-header">
                         <div className="item">
                             <div className="ui icon input">
                                 <input type="text" placeholder="Search..."/>
                                 <i aria-hidden="true" className="search icon"></i>
                             </div>
                         </div>
-                        {/*<a className="item">Logout</a>*/}
                     </div>
+                    <div className={`ui pointing vertical menu menu-list`} id={activeMenu ? "" : "menu-list-hidden"}>
+                        <a className="item" href="#" onClick={ navigFunc('/startups') }>Startups</a>
+                        <a className="item" href="#" onClick={ navigFunc('/create-new-startup') }>Create new</a>
+                        <a className="item" href="#" onClick={ navigFunc('/news') }>News</a>
+                        <div className="ui icon input" id="search-input-menu">
+                            <input type="text" placeholder="Search..."/>
+                            <i aria-hidden="true" className="search icon"></i>
+                        </div>
+                    </div>
+                    <div className="burger-menu" onClick={()=> setActiveMenu(!activeMenu)}>
+                        <img src="/img/burger.png" alt=""/>
+                    </div>
+
                 </div>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import 'semantic-ui-css/semantic.min.css'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'semantic-ui-css/semantic.min.css'
 import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
